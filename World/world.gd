37daf175dd6532ce8cb87f64cf8837c2df432835
googeris <162590514+googeris@users.noreply.github.com>
2024-03-07 18:43:16 +0200

    gen

--- World/world.gd
+++ World/world.gd
@@ -1,7 +1,9 @@
 extends Node2D
 
 @onready var tilemap = $TileMap
-const size = Vector2(20, 20)
+@onready var character_body = $CharacterBody2D
+const size = Vector2(100, 100)
+var r = RandomNumberGenerator.new()
 
 func _ready():
 	tilemap.clear()
@@ -11,4 +13,10 @@ func generate_map():
 	for x in size.x:
 		for y in size.y:
 			tilemap.set_cell(0, Vector2(x, y), 1, Vector2(0, 0), 0)
+	pass
+
+func set_spawn_point():
+	var x = r.randi_range(0, size.x-1)
+	var y = r.randi_range(0, size.y-1)
+
 	pass
