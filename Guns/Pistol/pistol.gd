70e37218a7be5a03e7d0d1f411476c7dea4e8160
mixeriss <112931161+mixeriss@users.noreply.github.com>
2024-03-11 18:57:33 +0200

    aim + shoot system

---
+++ Guns/Pistol/pistol.gd
@@ -0,0 +1,18 @@
+extends Node2D
+
+@onready var shootingpoint = %shootingPoint
+@onready var firerate = %firerate
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	look_at(get_global_mouse_position())
+
+func shoot():
+	if firerate.is_stopped():
+		const PROJECTILE = preload("res://Guns/Pistol/pistolProjectile.tscn")
+		var newProjectile = PROJECTILE.instantiate()
+		newProjectile.global_position = shootingpoint.global_position
+		newProjectile.global_rotation = shootingpoint.global_rotation
+		shootingpoint.add_child(newProjectile)
+		firerate.start()
--- Player/player.gd
+++ Player/player.gd
@@ -6,11 +6,14 @@ extends CharacterBody2D # shows what type of object the code belongs to
 @export var CROUCH_SPEED = 175.0
 @export var PRONE_SPEED = 80.0
 
+# access unique objects
+@onready var pistol = %Pistol
+
 # variables that are to be changed during runtime
 var currentSpeed = NORMAL_SPEED # assigns the default speed value
 
 func _physics_process(delta): # code in "_physics_process" is run every frame
-	
+	#look_at(get_global_mouse_position())
 	# switching movement speed depending on input
 	if Input.is_action_pressed("sprint"):
 		currentSpeed = SPRINT_SPEED
@@ -27,7 +30,10 @@ func _physics_process(delta): # code in "_physics_process" is run every frame
 	# settings >> input map beforehand
 	var direction = Input.get_vector("left", "right", "up", "down") 
 	velocity = direction * currentSpeed
-
+	
+	if Input.is_action_pressed("shoot gun"):
+		pistol.shoot()
+	
 	move_and_slide() # in-built method for detecting collisions and making the object slide around
 	# the collisioned object
 
