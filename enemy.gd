1ab2c8c53949f3f56b328372261f5b0ad79e5cd7
mixeriss <112931161+mixeriss@users.noreply.github.com>
2024-05-28 00:35:55 +0300

    HEALTH

--- enemy.gd
+++ enemy.gd
@@ -15,6 +15,9 @@ func change_multiplier(wave):
 	SPEED = SPEED * log(wave+1)
 	HEALTH = HEALTH * log(wave+1)
 
+func _ready():
+	HEALTH = 50.0
+
 func _physics_process(delta):
 	var direction = global_position.direction_to(player.global_position)
 	velocity = direction * SPEED
